5f788e78c606c1fd3163379411ff0f7bfde00f93
Curt Bushko <cbushko@gmail.com>
2022-12-06 23:31:56 -0500

    add a stack data structure using generics

---
+++ structure/stack/stack.go
@@ -0,0 +1,73 @@
+package stack
+
+import "fmt"
+
+// Stack is a generic stack of times of any time T
+//
+//	Usage:
+//	s := Stack([]int{1,2,3})
+//	s := NewStack[int]()
+type Stack[T any] struct {
+	items []T
+}
+
+// IsEmpty checks to see if the stack is empty
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.items) == 0
+}
+
+// Len returns the length of the stack
+func (s *Stack[T]) Len() int {
+	return len(s.items)
+}
+
+// NewStack creates a new empty stack
+// Usage:
+// s := Stack([]int{1,2,3})
+// s := NewStack[int]()
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{
+		items: []T{},
+	}
+}
+
+// Peek looks at the item at the top of the stack. Returns nil if empty.
+func (s *Stack[T]) Peek() any {
+	if s.IsEmpty() {
+		return nil
+	}
+	return s.items[s.Len()-1]
+}
+
+// PushN pushes a number of items onto the stack
+func (s *Stack[T]) PushN(v []T) {
+	s.items = append(s.items, v...)
+}
+
+// Push pushes a single item on the stack
+func (s *Stack[T]) Push(v T) {
+	s.items = append(s.items, v)
+}
+
+// PopN pops the number of items off of the stack. Return an error if popping more items than available.
+func (s *Stack[T]) PopN(n int) ([]T, error) {
+	var r []T
+	if s.Len() < n {
+		return r, fmt.Errorf("cannot pop %d items from stack of length %d", n, s.Len())
+	}
+
+	r = s.items[s.Len()-n:]
+	s.items = s.items[:s.Len()-n] // subject to memory leak
+	return r, nil
+}
+
+// Pop pops a single item off of the stack
+func (s *Stack[T]) Pop() any {
+	if s.IsEmpty() {
+		return nil
+	}
+	l := s.Len() - 1
+	i := s.items[l]
+	s.items = s.items[:l]
+	return i
+}
---
+++ structure/stack/stack_test.go
@@ -0,0 +1,51 @@
+package stack
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Stack(t *testing.T) {
+	// Create a new stack
+	s := NewStack[int]()
+
+	// Verify that the stack is empty
+	assert.True(t, s.IsEmpty(), "expected stack to be empty")
+
+	// Verify that the length of the stack is 0
+	assert.Equal(t, 0, s.Len(), "expected stack length to be 0")
+
+	// Push some itemsents onto the stack
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+
+	// Verify that the stack is not empty
+	assert.False(t, s.IsEmpty(), "expected stack to be not empty")
+
+	// Verify that the length of the stack is 3
+	assert.Equal(t, 3, s.Len(), "expected stack length to be 3")
+
+	// Verify that Peek() returns the top itemsent of the stack
+	assert.Equal(t, 3, s.Peek(), "expected Peek() to return 3")
+
+	// Verify that PushN() adds multiple itemsents to the stack
+	s.PushN([]int{4, 5, 6})
+	assert.Equal(t, 6, s.Len(), "expected stack length to be 6 after PushN()")
+
+	// Verify that Pop() removes and returns the top itemsent of the stack
+	item := s.Pop()
+	assert.Equal(t, 6, item, "expected Pop() to return 6")
+	assert.Equal(t, 5, s.Len(), "expected stack length to be 5 after Pop()")
+
+	// Verify that PopN() removes and returns multiple itemsents from the stack
+	items, err := s.PopN(3)
+	assert.Nil(t, err, "expected PopN() to return itemsents from the stack")
+	assert.Len(t, items, 3, "expected PopN() to return 3 itemsents")
+	assert.Equal(t, 2, s.Len(), "expected stack length to be 2 after PopN()")
+
+	// Verify that PopN() returns an error if called with n > s.Len()
+	_, err = s.PopN(3)
+	assert.NotNil(t, err, "expected PopN() to return an error when called with n > s.Len()")
+}
